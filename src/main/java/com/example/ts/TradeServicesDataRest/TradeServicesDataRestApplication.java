b7ad44bbc5de88c31a643f4b6d26b14648ba1862
Sahil Chopra <schopra18@sapient.com>
2018-06-03 13:16:02 +0530

    Initial commit

---
+++ src/main/java/com/example/ts/TradeServicesDataRest/TradeServicesDataRestApplication.java
@@ -0,0 +1,12 @@
+package com.example.ts.TradeServicesDataRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeServicesDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeServicesDataRestApplication.class, args);
+	}
+}
